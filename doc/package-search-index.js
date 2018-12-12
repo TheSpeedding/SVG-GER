f18147968314e006de549bc41d1fe98962c21605
Lukas Riedel <thespeedding@gmail.com>
2018-12-12 13:41:40 +0100

    Final commit.

--- doc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"svgger"},{"l":"svgger.commands"},{"l":"svgger.commands.expressions"},{"l":"svgger.commands.operators"},{"l":"svgger.commands.statements"},{"l":"svgger.commands.svg"},{"l":"svgger.stdlib"},{"l":"svgger.util"}]
